63e6b3ed4a4815cfe8628bc8e9c8a787a41fbf33
Jacob Kelley <exaybachay.ak@gmail.com>
2017-10-02 19:00:31 -0800

    Update Logging.ps1

--- Logging.ps1
+++ Logging.ps1
@@ -8,13 +8,14 @@ $diskinfo = Get-PSDrive C | Select-Object Used,Free
 $percentfree = $diskinfo.Free / ($diskinfo.Used + $diskinfo.Free)
 $diskinfosize = $diskinfo.Used + $diskinfo.Free
 $onefifth = ($diskinfosize/5)
-
+	
 #Get info about Event Logs
 $loginfo = Get-eventlog -list
 $seclog = $loginfo[8].MaximumKilobytes
 $syslog = $loginfo[9].MaximumKilobytes	
 $applog = $loginfo[0].MaximumKilobytes
 $pslog = $loginfo[10].MaximumKilobytes
+#$sysmonlog = $loginfo
 
 #Warning message to abort if disk space is low
 #1073741824 bytes is 1GB
@@ -26,6 +27,24 @@ if($diskinfo.Free -lt 1073741824){
 	else{
 		return
 	}
+}
+
+
+#####################################################################################
+###   Sysmon install and tweaks
+#####################################################################################
+# Further info on sysmon installation:
+# https://cqureacademy.com/blog/server-monitoring/sysmon
+
+& "$(pwd)\sysmon.exe" -accepteula -i -h md5 -l -n
+
+$sysmoninstalled = test-path "C:\Windows\System32\winevt\Logs\Microsoft-Windows-Sysmon%4Operational.evtx"
+
+if($sysmoninstalled -eq "True"){
+	write-host > "$(pwd)\extlogging.txt"
+}
+else{
+	
 }
 
 #####################################################################################
@@ -46,15 +65,9 @@ if($syslog -lt 307200){
 if($pslog -lt 307200){
 	limit-eventlog -logname "Windows Powershell" -MaximumSize 300MB		
 }
-
-#####################################################################################
-###   Sysmon install and tweaks
-#####################################################################################
-# Further info on sysmon installation:
-# https://cqureacademy.com/blog/server-monitoring/sysmon
-
-& "$(pwd)\Sysmon\sysmon.exe" -accepteula -i -h md5 -l -n
-
+if($pslog -lt 307200){
+	limit-eventlog -logname "Windows Powershell" -MaximumSize 300MB		
+}
 
 #####################################################################################
 ###   Registry mods for Powershell logging
@@ -62,17 +75,48 @@ if($pslog -lt 307200){
 # Credit to matthewdunwoody for the registry imports
 # https://github.com/matthewdunwoody/PS_logging_reg
 
-# First thing it to check status of current powershell environment
-# 
+# Get info about installed versions of PowerShell
+# Credit to Jaykul for script that grabs .NET Version
+# https://stackoverflow.com/questions/3487265/powershell-script-to-return-versions-of-net-framework-on-a-machine
 
-#& "C:\Program Files\Google\Chrome\Application\chrome.exe" --load-extension="$(pwd)\IPINT-master"
-& regedit "$(pwd)\PS_logging_reg-master\PS_logging.reg"
-
-$sysmoninstalled = test-path "C:\Windows\System32\winevt\Logs\Microsoft-Windows-Sysmon%4Operational.evtx"
-
-if($sysmoninstalled -eq "True"){
-	write-host > "$(pwd)\extlogging.txt"
+$dotnetver = Get-ChildItem 'HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP' -recurse |
+Get-ItemProperty -name Version,Release -EA 0 |
+Where { $_.PSChildName -match '^(?!S)\p{L}'} |
+Select PSChildName, Version, Release, @{
+  name="Product"
+  expression={
+      switch -regex ($_.Release) {
+        "378389" { [Version]"4.5" }
+        "378675|378758" { [Version]"4.5.1" }
+        "379893" { [Version]"4.5.2" }
+        "393295|393297" { [Version]"4.6" }
+        "394254|394271" { [Version]"4.6.1" }
+        "394802|394806" { [Version]"4.6.2" }
+        "460798" { [Version]"4.7" }
+        {$_ -gt 460798} { [Version]"Undocumented 4.7 or higher, please update script" }
+      }
+    }
 }
+
+# Sort table in descending order
+$dotnetver = $dotnetver | sort Version
+
+# Grab the largest entry in the table for reference
+$dotnetcheck = $dotnetver.Version[$dotnetver.length-1]
+
+if($dotnetcheck -ge 4.5){
+	# Check for WMF 4.0 and WMF 5.0 - Should be PSVersion 5 or above
+	$version = $PSVersionTable.PSVersion
+
+	if($version.Major -ge 5){
+		# Apply registry changes to enable extended logging
+		& regedit "$(pwd)\PS_logging.reg"
+	}
+	else{
+		write-host "You must upgrade your PowerShell (WMF) version to 5 or above to get full logging capabilities.  Please close this window, upgrade to version 5 and re-run this script."
+	}
+}
+
 else{
-	
+	write-host "You must upgrade your .NET version to 4.5 or above to get full logging capabilities.  Please close this window, upgrade to version 4.5 and re-run this script."
 }
