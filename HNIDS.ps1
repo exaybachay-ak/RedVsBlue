e5f695252abb6a374170e9f0f3d484668219b2bf
Jacob Kelley <exaybachay.ak@gmail.com>
2018-02-06 21:16:13 -0900

    Update HNIDS.ps1
    IP Scanning partially working.. I believe the API limit has been reached, so I will try again tomorrow and finalize MD5 functionality if all goes well

--- HNIDS.ps1
+++ HNIDS.ps1
@@ -1,50 +1,29 @@
 #####################################################################################
-###   Set up variables
+###   Set up variables, API key, and import modules
 #####################################################################################
 $Yesterday = (Get-Date) - (New-TimeSpan -Day 1)
 $FormatEnumerationLimit=-1
+#$apikey = Read-Host -Prompt "HNIDS.ps1 relies on VirusTotal intelligence.  Enter your VirusTotal API Key to continue"
+$apikey = "7ceaa6f71020d0b143ba0e05cc3619865679a24bdebfe01b5a5e92acd760ac02"
+if (!$apikey -or $apikey.length -lt 64){
+	write-output "You must enter a valid API key to use this script.  Please retry, using a valid VT API Key"
+	exit
+}
+
+#Import VirusTotal API module
+# CREDIT TO David B Heise
+#   https://archive.codeplex.com/?p=psvirustotal
+import-module .\psvirustotal\VirusTotal.psm1
+
+#Import SQLLite module
+# 
+#   https://github.com/RamblingCookieMonster/PSSQLite
+Import-module .\PSSQLite-master\PSSQlite\PSSQLite.psm1
+Install-module PSSQLite
 
 #####################################################################################
 ###   Get network connections from Sysmon event log
 #####################################################################################
-#Get list of suspicious IP Addresses to scan
-if(test-path .\blacklist.txt){
-	write-output "Looks like you already have the blacklist"
-	#Filter out non-ip addresses from list
-	$blacklist = get-content blacklist.txt | select-string -pattern "^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$" | sort-object | Get-Unique
-}
-
-if((test-path .\blacklist.txt) -eq $False){
-	#Set error action to silent, to suppress info
-	$ErrorActionPreference = "SilentlyContinue"
-
-	#Notify user that we are downloading blacklists
-	write-output "Downloading blacklists... please wait..."
-
-	#Retreive blacklist hosts from various sources
-	$greensnow = (invoke-webrequest -URI "blocklist.greensnow.co/greensnow.txt" -UseBasicParsing -TimeoutSec 60)
-	$bambenek = (invoke-webrequest -URI "osint.bambenekconsulting.com/feeds/c2-ipmasterlist.txt" -UseBasicParsing -TimeoutSec 60)
-	$alienvault = (invoke-webrequest -URI "https://reputation.alienvault.com/reputation.unix" -UseBasicParsing -TimeoutSec 60)
-	$binarybanlist = (invoke-webrequest -URI "https://www.binarydefense.com/banlist.txt" -UseBasicParsing -TimeoutSec 60)
-	$binarytorlist = (invoke-webrequest -URI "https://www.binarydefense.com/tor.txt" -UseBasicParsing -TimeoutSec 60)
-	$blocklistde = (invoke-webrequest -URI "https://lists.blocklist.de/lists/all.txt" -UseBasicParsing -TimeoutSec 60)
-
-	#Echo all ip addresses out into a full blacklist file
-	$greensnow.rawcontent > blacklist.txt
-	$banbenek.rawcontent >> blacklist.txt
-	$alientvault.rawcontent >> blacklist.txt
-	$binarybanlist.rawcontent >> blacklist.txt
-	$binarytorlist.rawcontent >> blacklist.txt
-	$blocklistde.rawcontent >> blacklist.txt
-
-	#Filter out non-ip addresses from list
-	$blacklist = get-content blacklist.txt | select-string -pattern "^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$" | sort-object | Get-Unique
-
-	#Notify user that setup tasks are done
-	write-output "Done downloading and setting up blacklist files"
-}
-
-#Get events from Sysmon event logs
 $netevents = Get-WinEvent -FilterHashTable @{ logname = "Microsoft-Windows-Sysmon/Operational"; Id = 3; StartTime=$Yesterday } -erroraction silentlycontinue | Where-Object {$_.Message -Like "*DestinationIP*" } | fl * | out-string | select-string -Pattern "DestinationIP..[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}" -All | Select Matches | sort-object | get-unique
 
 #Get full array without truncation, then put a string out
@@ -70,10 +49,169 @@ foreach($ip in $neteventssplit)
 #Dedupe info
 $netarr = $netarr | sort-object | get-unique
 
-foreach($ipaddy in $netarr){
-	write-output "Checking for $ipaddy"
-	$blacklist | sls $ipaddy
+#Display a gridview of detected IP Addresses
+#$netarr | out-gridview
+
+
+#Create a bunch of hashtables to store VT scan results
+$iplist = @{}
+$countrylist = @{}
+$ASOwnerlist = @{}
+$Resolutionlist = @{}
+$DetectedURLlist = @{}
+$DetectedDownloaded = @{}
+$UndetectedURLlist = @{}
+
+foreach($ip in $netarr){
+	$report = Get-VTReport -VTApiKey $apikey -ip $ip
+
+	#sort the resolutions out by last_resolved date, so we can grab latest one
+	$resolution = $report.resolutions | sort-object -property last_resolved -descending
+
+	$iplist.Add($netarr.IndexOf($ip), $ip)
+	$countrylist.Add($netarr.IndexOf($ip), $report.country)
+	$ASOwnerlist.Add($netarr.IndexOf($ip), $report.as_owner)
+	$Resolutionlist.Add($netarr.IndexOf($ip), $resolution[0])
+	$DetectedURLlist.Add($netarr.IndexOf($ip), $report.detected_urls[0])
+	$UndetectedURLlist.Add($netarr.IndexOf($ip), $report.undetected_urls[0])
+	$DetectedDownloaded.Add($netarr.IndexOf($ip), $report.detected_downloaded_samples[0])
 }
+
+$scaninfo = @()
+foreach($ip in $netarr){
+	$hash = New-Object PSObject -property @{Index=$netarr.IndexOf($ip);IP=$ip;Country=$countrylist[$netarr.IndexOf($ip)];ASOwner=$ASOwnerList[$netarr.IndexOf($ip)];Resolution=$ResolutionList[$netarr.IndexOf($ip)];DetectedURL=$DetectedURLlist[$netarr.IndexOf($ip)];UndetectedURLs=$UndetectedURLlist[$netarr.IndexOf($ip)];DetectedMalware=$DetectedDownloaded[$netarr.IndexOf($ip)]}
+	$scaninfo += $hash
+}
+
+#Display a grid view of all VT data
+$scaninfo | select-object Index,IP,Country,ASOwner,DetectedMalware,Resolution,DetectedURL,UndetectedURLs | out-gridview
+
+<#
+#########################################################
+# Create a SQLite database to store VirusTotal information
+#########################################################
+$Query = "CREATE TABLE NAMES (fullname VARCHAR(20) PRIMARY KEY, surname TEXT, givenname TEXT, BirthDate DATETIME)"
+$DataSource = "$pwd\VirusTotal.SQLite"
+
+Invoke-SqliteQuery -Query $Query -DataSource $DataSource
+
+#########################################################
+# Use nested list/array
+#########################################################
+
+foreach($ip in $netarr){
+	$report = Get-VTReport -VTApiKey $apikey -ip $ip
+	$vtlist += ,@($netarr,$netarr.IndexOf($ip),$report.country,$report.as_owner,$report.resolutions,$report.detected_urls)
+}
+
+
+#########################################################
+# Use multidimensional hashtable
+#########################################################
+foreach($ip in $netarr){
+	$report = Get-VTReport -VTApiKey $apikey -ip $ip
+	$vtarray[$netarr.IndexOf($ip)] = @{}
+	$vtarray[$netarr.IndexOf($ip)]["IPAddress"] = $ip
+	$vtarray[$netarr.IndexOf($ip)]["Country"] = $report.country
+	$vtarray[$netarr.IndexOf($ip)]["ASOwner"] = $report.as_owner
+	$vtarray[$netarr.IndexOf($ip)]["Resolutions"] = $report.resolutions
+	$vtarray[$netarr.IndexOf($ip)]["DetectedURLs"] = $report.detected_urls
+}
+
+#$vtarray["IPAddress"] = @{}
+#$vtarray["IPAddress"]["Country"] = @{}
+#$vtarray["IPAddress"]["ASOwner"] = @{}
+#$vtarray["IPAddress"]["Resolutions"] = @{}
+#$vtarray["IPAddress"]["DetectedURLs"] = @{}
+#$vtarray["IPAddress"]["DetectedCommunicatingSamples"] = @{}
+#$vtarray["IPAddress"]["UnDetectedCommunicatingSamples"] = @{}
+#$vtarray["IPAddress"]["DetectedDownloadedSamples"] = @{}
+#$vtarray["IPAddress"]["UnDetectedDownloadedSamples"] = @{}
+#$vtarray["IPAddress"]["ResponseCode"] = @{}
+#$vtarray["IPAddress"]["VerboseMessage"] = @{}
+#$vtarray["IPAddress"]["ASN"] = @{}
+
+for($counter=0; $counter -lt $netarr.length; $counter++) {
+	write-output "Scanning $netarr[$counter]"
+	$report = Get-VTReport -VTApiKey $apikey -ip $netarr[$counter]
+
+	write-output $report.country
+	write-output $report.asowner
+	write-output $report.resolutions
+	write-output $report.detected_urls
+
+	$vtarray[$counter] = @{}
+	$vtarray[$counter]["IPAddress"] = $netarr[$counter]
+	$vtarray[$counter]["Country"] = $report.country
+	$vtarray[$counter]["ASOwner"] = $report.asowner
+	$vtarray[$counter]["Resolutions"] = $report.resolutions
+	$vtarray[$counter]["DetectedURLs"] = $report.detected_urls
+
+	#$vtarray["Entry"][$counter]["IPAddress"] = $ipaddy
+	#$vtarray["Entry"][$counter]["Country"] = $report.country
+	#$vtarray["Entry"][$counter]["ASOwner"] = $report.as_owner
+	#$vtarray["Entry"][$counter]["Resolutions"] = $report.resolutions
+	#$vtarray["Entry"][$counter]["DetectedURLs"] = $report.detected_urls
+	
+	#$vtarray.Add('Country')
+
+	$vtarray[$ipaddy] = $ipaddy
+	$vtarray[$ipaddy]["Country"] = $report.country
+	$vtarray[$ipaddy]["ASOwner"] = $report.as_owner
+	$vtarray[$ipaddy]["Resolutions"] = $report.resolutions
+	$vtarray[$ipaddy]["DetectedURLs"] = $report.detected_urls
+	$vtarray[$ipaddy]["DetectedCommunicatingSamples"] = $report.detected_communicating_samples
+	$vtarray[$ipaddy]["UndetectedCommunicatingSamples"] = $report.undetected_communicating_samples
+	$vtarray[$ipaddy]["DetectedDownloadedSamples"] = $report.detected_downloading_samples
+	$vtarray[$ipaddy]["UndetectedDownloadedSamples"] = $report.undetected_downloading_samples
+	$vtarray[$ipaddy]["ResponseCode"] = $report.response_code
+	$vtarray[$ipaddy]["VerboseMessage"] = $report.verbose_msg
+	$vtarray[$ipaddy]["ASN"] = $report.asn
+
+	#$lastresolved = $report.resolutions | sort-object -property last_resolved -descending
+	#write-output "The last known hostname is $lastresolved[0].hostname"
+	#write-output "This system is owned by $report.as_owner"
+
+	#Come up with a mixture of conditions that should alert someone of suspicious activity
+	#$detectioncheck = $report | sls -Pattern "positives=[2-9]{1,2}" -all | Select Matches | fl * | out-string
+
+	#If detection rules match, notify user somehow
+	#if($detectioncheck){
+	#
+	#}
+}
+
+$vtarray | out-gridview
+
+VT API Columns:::
+country
+as_owner
+
+resolutions
+detected_urls
+detected_downloaded_samples
+undetected_downloaded_samples
+detected_communicating_samples
+undetected_communicating_samples
+response_code
+verbose_msg
+asn
+
+
+Testing network scan to grab anything that resolves over 5 times
+-also need to think of other suspicious things like country=netherlands, or ..
+
+#Run report on IP Address
+$VTScan = Get-VTReport -VTApiKey $apikey -ip 8.8.8.8
+
+#Change into a string for grepping
+$VTScanString = $VTScan | out-string
+
+#Look for all matches, to determine if it's worth warning about
+$Scanmatches = $VTScanString | sls -Pattern "positives=[2-9]{1,2}" -all | Select Matches
+
+
+
 
 #####################################################################################
 ###   Get file info from Sysmon event log
@@ -105,14 +243,4 @@ foreach($md5 in $md5eventssplit)
 
 #Dedupe info
 $md5arr = $md5arr | sort-object | get-unique
-
-
-#########################################################################################
-###   Scan MD5 List With VirusTotal API
-#########################################################################################
-#Take MD5 list and scan with VirusTotal API
-#Install Posh-VirusTotal module
-iex (New-Object Net.WebClient).DownloadString("https://gist.githubusercontent.com/darkoperator/9138373/raw/22fb97c07a21139a398c2a3d6ca7e3e710e476bc/PoshVTInstall.ps1")
-
-#Set VirusTotal API Key
-Set-VTAPIKey -APIKey efffc8248da060451491096897a3d4a05f6d0641c9eb93a54c3f95e46411f27e -MasterPassword What_The_Fuck_THISISBS$#@!
+#>
