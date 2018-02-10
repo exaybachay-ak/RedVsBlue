01d6875b82feaf0d84e59bf6ffa01a80e78b460e
Jacob Kelley <exaybachay.ak@gmail.com>
2018-02-09 23:39:58 -0900

    Update Sinkhole.ps1

--- Sinkhole.ps1
+++ Sinkhole.ps1
@@ -2,32 +2,17 @@
 
       #Powershell blackhole script
           Just run this script and it will re-route potentially malicious traffic to null
-          In order to verify this, try run a to traceroute to 64.182.208.181 (<-- update this address later)
-            Your traceroute should route to null because this software has configured a rule to force specific outbound
+          In order to verify this, try run a to ping/pathping/traceroute to 64.182.208.181
+            Your trace should route to null because this software has configured a rule to force specific outbound
             traffic to the localhost bitbucket
-          If you want to un-do the changes, select the reverse changes option and it will roll things back.  If something
-          went wrong and the rules are "stuck" on, then re-run this script with the -undo switch.  This will go in and
-          remove any settings that the script would have implemented, basically by running the script in reverse.
-  
+          If you wan`t to un-do the changes, select option 2 to roll things back.  
+          
       NOTE: At least half of the credit should go to Dave Kennedy and @BinaryDefense for making GoatRider.  
              Although the main component is the idea of re-routing to null, and the ease of use, GoatRider was what brought it all together
+             https://github.com/BinaryDefense/goatrider/blob/master/goatrider.py
   
-      #Program flow:
-      To bounce malicious inbound/outbound traffic:  Run bounce.ps1
-      To undo change made previously by this script: Run bounce.ps1 -undo
       #Primary powershell command
       New-NetRoute -DestinationPrefix "152.195.54.20/32" -InterfaceIndex 1 -NextHop 127.0.0.1
-      #OSINT Threat Sources:
-      https://www.esentire.com/news-and-events/press-releases/esentire-launches-largest-open-source-threat-intelligence-aggregator/
-      https://github.com/hslatman/awesome-threat-intelligence
-      http://osint.bambenekconsulting.com/feeds/c2-ipmasterlist.txt
-      http://blocklist.greensnow.co/greensnow.txt
-      https://intel.criticalstack.com/user/sign_up
-      https://github.com/BinaryDefense/goatrider/blob/master/goatrider.py
-      *more to be added later*
-      #Known hosting platform IP blocks
-      http://docs.aws.amazon.com/general/latest/gr/aws-ip-ranges.html
-      http://www.senderbase.org/lookup/org/?search_string=OVH%20SAS
       
 #>
 
